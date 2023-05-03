598d737b3c227a06d840392bcc0f2cd2287663bb
Stanley Shrewsbery <homestanrunner@gmail.com>
2023-05-03 10:43:38 -0400

    create working cardBuilder function

--- index.js
+++ index.js
@@ -5,15 +5,33 @@ fetch("http://localhost:3000/cookies")
 .then(cookie => cardBuilder(cookie))
 
 //functions
-//Dom Manipulation
-const cardBuilder = (dataArray) => {
-  console.log(dataArray)
-  dataArray.forEach(cardInfo => {
+const cardBuilder = (cookie) => {
+  console.log(cookie)
+  cookie.forEach(cardInfo => {
+      const cardContainer = document.getElementById("cookie-container")
+    const cookieCard = document.createElement("div")
     const img = document.createElement("img")
     img.src = cardInfo.image
-    const cardContainer = document.getElementById("cookie-container")
+    const flavor = document.createElement("h3")
+    flavor.innerText = cardInfo.flavor
+    const p = document.createElement("p")
+    p.textContent = cardInfo.description
+    const selectArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    const userRating = document.createElement("select");
+    for(let i = 0; i < 10; i++) {
+        let option = document.createElement("option");
+        option.value = selectArray[i];
+        option.text = selectArray[i];
+        userRating.appendChild(option);
+    }
+    userRating.value = cardInfo.rating;
 
-    cardContainer.appendChild(img)
+
+    cookieCard.appendChild(flavor)
+    cookieCard.appendChild(img)
+    cookieCard.appendChild(p)
+    cookieCard.appendChild(userRating)
+    cardContainer.appendChild(cookieCard)
   })
 }
 //place cookies in the HTML using cardBuilder
